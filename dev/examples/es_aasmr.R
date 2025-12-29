4737898344501f3ab120d0dd40e5f4a7e28ee72e
Stan Traykov <st@gmuf.com>
2025-12-29 12:30:24 +0200

    add starting year setting in es aasmr example

--- dev/examples/es_aasmr.R
+++ dev/examples/es_aasmr.R
@@ -1,19 +1,21 @@
 library(tidyverse)
 source("dev/examples/common_spec.R")
 
+start_from <- 2025
+
 specs <- tribble(
-    ~geo, ~sex, ~age,
-    "BG", "m", 42L,
-    "BG", "f", 42L,
-    "NL", "m", 42L,
-    "NL", "f", 42L,
+    ~geo, ~sex, ~age, ~start,
+    "BG", "m", 42L, start_from,
+    "BG", "f", 42L, start_from,
+    "NL", "m", 42L, start_from,
+    "NL", "f", 42L, start_from
 )
 
 es <- read_es_aasmr(file.path("data", "life"))
 
 specs %>%
     mutate(
-        ca = pmap(list(geo, sex, age), \(geo, sex, age) chance_alive_es_aasmr(es, geo, sex, age)),
+        ca = pmap(list(geo, sex, age, start), \(geo, sex, age, start) chance_alive_es_aasmr(es, geo, sex, age, start)),
         p  = pmap(list(ca, sex, geo), plot_chance_alive_es_aasmr),
         file_id = glue("esp_{geo}_{sex}_{age}")
     ) %>%
